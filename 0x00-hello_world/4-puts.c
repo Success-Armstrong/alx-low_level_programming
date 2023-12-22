d11abdd9170ad930a4bd372dad7d72f9305e26ef
Success-Armstrong <njokusuccess71@gmail.com>
2023-12-22 00:48:32 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,8 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual
- * puzzle followed by a newline.
- * Return: Always 0
+ * main - A program that prints a line with puts function 
+ * Return: 0 (Success)
  */
 
 
