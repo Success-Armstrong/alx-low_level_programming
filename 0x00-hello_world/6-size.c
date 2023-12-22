80ef786aa80cb2b7e5429b8b5b8a7291d9d9262b
Success-Armstrong <njokusuccess71@gmail.com>
2023-12-22 02:16:01 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@ int main(void)
 	char a;
         int b;
         long int c;
-        long long d
+        long long d;
         float f;
 
 printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(a));
